a33b189244587811b62a9656a7558add411e7125
John Paul Bamberg <jpbamberg1993@gmail.com>
2017-01-10 20:34:37 -0500

    Add semi colon to the end of return statement

--- app/routes/programmers.js
+++ app/routes/programmers.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ["Larry Wall", "Linus Torvalds", "Grace Hopper"]
+    return ["Larry Wall", "Linus Torvalds", "Grace Hopper"];
   }
 });
